9ad2f869111877a1c88f221616e7e2587c599beb
Michelle Charron <michelle.charron93@gmail.com>
2021-01-22 16:12:13 -0700

    auth0 config

--- wwwsrc/src/AuthConfig.js
+++ wwwsrc/src/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-2rswc7ro.us.auth0.com'
+export const clientId = 'lv9Dd6oCcRXpV0O58eDXV8dR655fjRy1'
+export const audience = 'https://tasksmaster'
